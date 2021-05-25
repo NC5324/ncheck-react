e9bf9cc2e93963856d6a11f8f9b8c52bf9246496
NaidDzhan Durmush <nc.durmush@gmail.com>
2021-05-25 19:09:50 +0300

    add styled buttons

---
+++ src/ui-components.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const ButtonTemplate = styled.button`
+  padding: 10px;
+  margin-top: auto;
+  margin-bottom: auto;
+  
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  box-shadow: #121212 0 0 6px;
+  
+  flex: 0 1 130px;
+  height: min-content;
+  font-weight: bolder;
+
+  cursor: pointer;
+`
+
+export const Button = styled(ButtonTemplate)`
+  background-color: #1D3240;
+  :hover {
+    background-color: #223F53;
+  }
+`
+
+export const AddButton = styled(ButtonTemplate)`
+  background-color: #467C46;
+  :hover {
+    background-color: #417341;
+  };
+`
